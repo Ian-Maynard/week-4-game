b228b538bf73ad77e933fc83664c8cb14ac3c434
Ian Maynard0 <maynard.ian@gmail.com>
2017-03-15 16:02:13 -0400

    Multiple changes across all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,94 @@
 // Setup routines - Variable initialization 
 // disPlaytile(c); // Display gems 
 // disPlaytile(s); // Display scores 
-//  clear screen
+//  clear Values
 // INIT multi-game variables
 // var gameScores=0 
 // prompt while playgame=true  {
 // init game variables
 // console.log("  "+targetNumber+"  " +userNumber+"  "+chrysOne+"  "+chrysTwo+"  "+chrysThr+"  "+chrysFou+"  ");
 
-var targetNumber = Math.floor(Math.random() * ((120-19)+1)+19);
-var userNumber = Math.floor(Math.random() * ((12-1)+1)+1);
-var chrysOne= Math.floor(Math.random() * ((12-1)+1)+1);
-var chrysTwo= Math.floor(Math.random() * ((12-1)+1)+1);
-var chrysThr= Math.floor(Math.random() * ((12-1)+1)+1);
-var chrysFou= Math.floor(Math.random() * ((12-1)+1)+1);
+
+$(document).ready(function() {
+
+var gameWins=0;
+var gameLosses=0;
+var playGame= false;
 
 
+// This is the start of the current game loop 
+
+if (confirm("Do you want to Play Chrystals?" ) == true){
+   playGame = true;
+   console.log("1 Play Game is: ",playGame);
+}
 
 
+if (playGame==true) {
+
+   console.log("2 Play Game is: ",playGame);
+
+    var targetNumber = Math.floor(Math.random() * ((120-19)+1)+19);
+    var userNumber = 0;
+    var chrysOne = Math.floor(Math.random() * ((12-1)+1)+1);
+    var chrysTwo = Math.floor(Math.random() * ((12-1)+1)+1);
+    var chrysThr = Math.floor(Math.random() * ((12-1)+1)+1);
+    var chrysFou = Math.floor(Math.random() * ((12-1)+1)+1);
+    var letterBtn = $("<button>");
+    
+
+      if (userNumber < targetNumber)  {
+
+        //display current totals 
+
+            $("div.targetNum").text(targetNumber);
+            $("div.curTotal").text(userNumber);
+            $("div.winLoss").text("Wins: "+gameWins+" / "+" Losses: "+gameLosses);
 
 
-
-
-      while userNumber < targetNumber  {
-
-//               // accept button1  input
-//               // button value
+            
+               // accept button1  input
+            $("#billy1").on("click",function(){
             userNumber = userNumber + chrysOne;
-
-//               // accept button2  input
+            console.log(" 3 Billy 1");
+            });
+            // accept button2  input
+            $("#billy2").on("click",function(){
             userNumber = userNumber + chrysTwo;
-
-//               // accept button3  input
+            console.log("Billy 2");
+            });
+            // accept button3  input
+            $("#billy3").on("click",function(){
             userNumber = userNumber + chrysThr;
-//               // accept button4  input
-//               // increment Gamescore by chrystal
-//               // button value
+            console.log("Billy 3");
+            });    
+            // accept button4  input
+            $("#billy4").on("click",function(){
+            userNumber = userNumber + chrysFou;
+            console.log("Billy 4");
+            }); 
 
 
-              if userNumber > targetNumber {
-                gameResult(userNumber,targetnumber,playGame);
-                playGame = false;
+            if (userNumber > targetNumber) {
+                alert("YOU LOSE!");
+                gameLosses++;
               }
 
-              if userNumber = targetNumber {
-                gameResult(userNumber,targetnumber,playGame);
-                playGame = false;
+            if (userNumber = targetNumber) {
+                 alert("YOU're MAHVELUS! You win!");
+                 console.log(userNumber,targetNumber);
+                 gameWins++;     
               }
-
 
           }  // End current game loop 
 
+          if (confirm("Do you want to keep playing?" ) =! true){
+            playGame = false;
+            console.log("1 Play Game is: ",playGame);
+            }
 
+}
 
 }  // End Multigame loop 
 
-
-
-
-function printScores(){
-for (var i = 1; i <= (gameScores[i]-1); i++) {
-    var letterBtn = $("<button>");
-      letterBtn.addClass("letter-button letter letter-button-color");
-       letterBtn.attr("data-letter", 'gameWord[i]');
-       letterBtn.text("gameWord[i]");
-       $("#buttons").append(letterBtn);
-      }
-}
-
-function disPlaytile(){
-// Assign print score default
-$("#gems").empty(); // Clear the display area 
-gemURL="";
-
-    for (var i = 0; i <= 3; i++) {
-      var GemBtn = $("<gems>");
-          GemBtn.addClass("gem-button");
-       	  GemBtn.attr("data-letter", gemUrl);
-       	  $("#buttons").append(gemBtn);
-// Generate and display current character
-      }
- // Display Message
+} // End of function wrapper
